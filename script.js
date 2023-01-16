6d29d498ecf89137dd259a8ad5390ff00c44562d
Lakshman Kumar <118086762+lakshmankumar22@users.noreply.github.com>
2023-01-17 00:46:41 +0530

    add all files without folder
    Add all html, css, js, img and pdf file without img folder source

---
+++ script.js
@@ -0,0 +1,281 @@
+function selectCollegeName() {
+  let e = document.querySelector("#college");
+  let val = e.value;
+  let text = e.options[e.selectedIndex].text;
+  document.querySelector("#selected_college").innerHTML = text;
+  document.querySelector(".card_college_name").innerHTML = val;
+}
+
+selectCollegeName();
+
+document.querySelector("#college").addEventListener("click", function () {
+  selectCollegeName();
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+let navbar = document.getElementById("navbar");
+let homeSec = document.getElementById("home");
+
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    homeSec.classList.add("pad-top");
+  } else {
+    navbar.classList.remove("sticky");
+    homeSec.classList.remove("pad-top");
+  }
+}
+
+// menu button toggle
+document.querySelector(".cross").style.display = "none";
+document.querySelector(".ham").addEventListener("click", () => {
+  document.querySelector("#menu_icon").classList.toggle("hamburger");
+  if (document.querySelector("#menu_icon").classList.contains("hamburger")) {
+    document.querySelector(".ham").style.display = "inline";
+    document.querySelector(".cross").style.display = "none";
+  } else {
+    document.querySelector("#menu_icon").classList.toggle("hamburger");
+    document.querySelector(".ham").style.display = "none";
+    setTimeout(() => {
+      document.querySelector(".cross").style.display = "inline";
+    }, 100);
+  }
+});
+
+document.querySelector(".cross").addEventListener("click", () => {
+  if (document.querySelector("#menu_icon").classList.contains("hamburger")) {
+    document.querySelector(".ham").style.display = "inline";
+    document.querySelector(".cross").style.display = "none";
+  } else {
+    document.querySelector(".ham").style.display = "none";
+    document.querySelector(".cross").style.display = "inline";
+  }
+});
+
+// navlist show hide
+burger = document.querySelector(".hamburger");
+navbar = document.querySelector(".navbar");
+navList = document.querySelector(".navlist");
+icon = document.querySelector("#navlist_branch .icons");
+
+burger.addEventListener("click", () => {
+  navList.classList.toggle("v_class");
+  navbar.classList.toggle("h_nav");
+  icon.classList.toggle("v_class");
+});
+
+// for length of 4 for branch
+let numberOfBranch = document.querySelectorAll(".branch_box").length;
+
+// for length of 7 for syllabus
+let numberOfSyllabus = document.querySelectorAll(".sbd_column").length;
+
+// for length of 7 for semester list item
+let numberOfSemesterListItem =
+  document.querySelectorAll(".semester_list li").length / 4;
+
+// for length of 4 for semester list
+let numberOfSemesterList = document.querySelectorAll(".semester_list").length;
+
+// for length of 7 for share button
+let numberOfShare = document.querySelectorAll(".sbd_share").length;
+
+// input value change
+let inputSelect = document.querySelector(".share_copy input");
+
+// select all share button
+const sharebtn = document.querySelectorAll(".sbd_share");
+
+// set localhost
+let localHost = "http://127.0.0.1:5500/";
+
+// syllabus time for select which year or which class syllabus is
+let syllabusTime = [
+  "year_1st",
+  "semester_3rd",
+  "semester_4th",
+  "semester_5th",
+  "semester_6th",
+  "semester_7th",
+  "semester_8th",
+];
+
+// branch name declare
+
+let branchBox = ["civil", "computer", "ece", "mechanical"];
+
+// scale ka khel
+
+for (let i = 0; i < numberOfSemesterList; i++) {
+  let docSelec = branchBox[i];
+  let docSelecName = "#semester_list_" + docSelec + " li";
+  for (let i = 0; i < numberOfSemesterListItem; i++) {
+    let semTime = syllabusTime[i];
+    let semNum = "#syllabus_" + semTime;
+    document
+      .querySelectorAll(docSelecName)
+      [i].addEventListener("click", function () {
+        let ssC = document.querySelector(semNum);
+        ssC.classList.add("scale");
+        setTimeout(() => {
+          ssC.classList.remove("scale");
+        }, 2000);
+      });
+  }
+}
+
+let colName = document.querySelector("#college").value;
+
+for (let i = 0; i < numberOfBranch; i++) {
+  let branchBox = ["civil", "computer", "ece", "mechanical"];
+  let branchBoxName = branchBox[i];
+  console.log("branchboxname is : " + branchBoxName);
+  let syllabusBranchName =
+    branchBoxName.slice(0, 1).toUpperCase() +
+    branchBoxName.slice(1, branchBoxName.length).toLowerCase();
+  console.log("uppercase syllabusbranch name is: " + syllabusBranchName);
+
+  // yaha dot isliye diya gaya hai taki jab navigation bar wale branch par bhi click kiya jaye to ye work kare
+
+  let idBBN = "." + branchBoxName;
+  let classBBN = "#syllabus_card";
+  console.log(idBBN);
+  console.log(classBBN);
+
+  // jab kisi branch par click hoga tab ke liye
+  $(idBBN).click(function () {
+    console.log("syllabus branch name is: " + syllabusBranchName);
+    document.querySelector("#syllabus_branch").innerHTML =
+      syllabusBranchName + " Branch Syllabus";
+    $(".card_branch_name").text(syllabusBranchName);
+
+    // for 7
+    // select which syllabus year
+    for (let i = 0; i < numberOfSyllabus; i++) {
+      let syllabusTimeName = syllabusTime[i];
+      console.log("syllabus time name is:" + syllabusTimeName);
+
+      // select downloads anchor tag
+      let syllabusDownloads = document.querySelectorAll(".sbd_downloads a");
+      let syllabusDownloadsName = syllabusDownloads[i];
+      console.log("syllabus downloads name is :" + syllabusDownloadsName);
+
+      // select preview anchor tag
+      let syllabusPreview = document.querySelectorAll(".sbd_preview a");
+      let syllabusPreviewName = syllabusPreview[i];
+      console.log("syllabus preview name is :" + syllabusPreviewName);
+
+      let downloadNameSelect =
+        colName + "_" + syllabusBranchName + "_" + syllabusTimeName;
+      console.log(downloadNameSelect);
+
+      let pdfSelect = downloadNameSelect + ".pdf";
+      console.log(pdfSelect);
+
+      syllabusDownloadsName.setAttribute("href", pdfSelect);
+      syllabusDownloadsName.setAttribute("download", downloadNameSelect);
+      syllabusPreviewName.setAttribute("href", pdfSelect);
+
+      let inputValue = localHost + pdfSelect;
+
+      sharebtn[i].addEventListener("click", function () {
+        inputSelect.setAttribute("value", inputValue);
+      });
+    }
+  });
+}
+
+for (let i = 0; i < numberOfShare; i++) {
+  let syllabusTimeName = syllabusTime[i];
+  console.log("syllabus time name is:" + syllabusTimeName);
+
+  // syllabusTime();
+  let pdfSelect =
+    colName +
+    "_" +
+    "Civil_" +
+    syllabusTimeName +
+    ".pdf";
+  console.log(pdfSelect);
+
+  let inputValue = localHost + pdfSelect;
+
+  sharebtn[i].addEventListener("click", function () {
+    inputSelect.setAttribute("value", inputValue);
+    copyFunction();
+    document.querySelector(".share_popup").classList.toggle("hidden");
+  });
+
+  document.querySelector(".close").addEventListener("click", function () {
+    document.querySelector(".share_popup").classList.toggle("hidden");
+  });
+}
+
+// copy function for copy paste
+function copyFunction() {
+  let copyText = document.getElementById("myInput");
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+}
+
+// my copy function for copy paste and also text change from copy to copied
+function myCopyFunction() {
+  copyFunction();
+  let myCopyText = document.querySelector(".copy_btn");
+  myCopyText.innerHTML = "Copied";
+  setTimeout(() => {
+    myCopyText.innerHTML = "Copy";
+  }, 2000);
+}
+
+// contact
+
+function clearErrors() {
+  errors = document.getElementsByClassName("form-error");
+  for (let item of errors) {
+    item.innerHTML = "";
+  }
+}
+function seterror(id, error) {
+  // sets error inside tag of id
+  element = document.getElementById(id);
+  element.getElementsByClassName("form-error")[0].innerHTML = error;
+}
+
+function validateForm() {
+  let returnval = true;
+  clearErrors();
+
+  // Perform validation and if validation fails, set the value of returnval to false
+  let name = document.forms["myForm"]["name"].value;
+  if (name.length < 3) {
+    seterror("fname", "*Minimum Length should be 3");
+    returnval = false;
+  }
+
+  let email = document.forms["myForm"]["email"].value;
+  if (email.length < 12) {
+    seterror("femail", "*Length of email is too short!");
+    returnval = false;
+  } else if (email.length > 50) {
+    seterror("femail", "*Length of Email is too long!");
+    returnval = false;
+  }
+
+  let subject = document.forms["myForm"]["subject"].value;
+  if (subject.length < 5) {
+    seterror("fname", "*Minimum length should be 5");
+    returnval = false;
+  }
+
+  return returnval;
+}
